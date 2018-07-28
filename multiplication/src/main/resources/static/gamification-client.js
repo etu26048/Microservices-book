e06e65aca530349d52d5894ee4a499123df3d4c7
Lazra <Lazra@192.168.1.5>
2018-07-28 23:33:15 +0200

    adding ','

--- multiplication/src/main/resources/static/gamification-client.js
+++ multiplication/src/main/resources/static/gamification-client.js
@@ -12,7 +12,7 @@ function updateLeaderBoard() {
 
 function updateStats(userId) {
 	$.ajax({
-		url : 'http://localhost:8081/stats?userId=' + userId
+		url : 'http://localhost:8081/stats?userId=' + userId,
 		success: function(data) {
 			$('#stats-div').show();
 			$('#stats-user-id').empty().append(userId);
